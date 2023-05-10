f7fd8a10f76b45cc020425efa9a359882cf86d86
Andrey vmlyr <misael.andrey@gmail.com>
2023-05-09 23:22:56 -0500

    TEst 1

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+  
+
+def main():
+    ouput = jsonify({"message":"main is invalid"})    
+    print(ouput)
+
+"""
+main ...........................................................................
+"""
+if __name__ == '__main__':
+    main();
